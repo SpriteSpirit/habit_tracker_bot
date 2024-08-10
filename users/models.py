d04b69572b91ae8630f351ac836d9ae8f6c6454c
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-08-10 14:27:53 +0300

    Настроена команда для создания администратора

--- users/models.py
+++ users/models.py
@@ -3,4 +3,19 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    """ Пользователь """
+    username = None
+
+    email = models.EmailField(unique=True, verbose_name='Email')
+    first_name = models.CharField(max_length=100, verbose_name='Имя')
+    last_name = models.CharField(max_length=100, verbose_name='Фамилия')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.email
