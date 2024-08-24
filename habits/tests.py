4a4a30702d40b555d6a3ce852fdc035e30f893ab
Sprite_Spirit <74331153+SpriteSpirit@users.noreply.github.com>
2024-08-24 18:32:45 +0300

    Созданы тесты для привычки

--- habits/tests.py
+++ habits/tests.py
@@ -1,3 +1,87 @@
-from django.test import TestCase
+from django.core.management import call_command
+from rest_framework import status
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from habits.models import Habit
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+    """ Тестирование привычек """
+
+    def setUp(self) -> None:
+        call_command('loaddata', 'test_data.json')
+        self.user = User.objects.get(email='admin@localhost')
+        self.client = APIClient()
+        self.client.force_authenticate(user=self.user)
+
+        self.habit = Habit.objects.get(pk=1)
+
+    def test_create_habit(self):
+        """ Тестирование создания привычки """
+
+        data = {
+            'user': 1,
+            'action': 'Test action',
+            'place': 'Test place',
+            'time': '12:00',
+            'frequency': 1,
+            'execution_time': '01:00',
+            'is_pleasant': True,
+            'reward': None,
+            'linked_habit': None,
+            'is_public': False
+        }
+
+        response = self.client.post('', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.json()['action'], 'Test action')
+
+
+    def test_list_habits(self):
+        """ Тестирование получения списка привычек """
+
+        response = self.client.get('', format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['count'], 3)
+
+
+    def test_get_habit(self):
+        """ Тестирование получения информации о привычке """
+
+        response = self.client.get(f'/{self.habit.pk}/', format='json')
+        print(response.json())
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['action'], 'habit1')
+
+
+    def test_update_habit(self):
+        """ Тестирование изменения информации о привычке """
+
+        data = {
+            'action': 'habit1 updated',
+            'place': 'Test place updated',
+            'time': '13:00',
+            'frequency': 1,
+            'execution_time': '02:00',
+            'is_pleasant': True,
+            'reward': None,
+            'linked_habit': 2,
+            'is_public': True
+        }
+
+        response = self.client.put(f'/{self.habit.pk}/', data, format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.json()['action'], 'habit1 updated')
+
+
+    def test_delete_habit(self):
+        """ Тестирование удаления привычки """
+
+        response = self.client.delete(f'/{self.habit.pk}/', format='json')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
--- habits/views.py
+++ habits/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from rest_framework.response import Response
-from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework.views import APIView
 
 from habits.models import Habit
 from habits.permissions import IsOwnerOrReadOnly
@@ -10,12 +10,11 @@ from habits.serializers import UserHabitSerializer, HabitCreateSerializer
 
 class HabitViewSet(viewsets.ModelViewSet):
     """ Контроллер для управления привычками """
-
     queryset = Habit.objects.all()
     permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_serializer(self, *args, **kwargs):
-        if self.action == 'create':
+        if self.action in ['create', 'update']:
             return HabitCreateSerializer(*args, **kwargs)
         elif self.action in ['list', 'retrieve']:
             return UserHabitSerializer(*args, **kwargs)
@@ -23,27 +22,23 @@ class HabitViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         """ Сохраняет привычку пользователя """
-
         serializer.save(user=self.request.user)
 
     def get_queryset(self):
         """ Получает привычки, отфильтрованные по параметру запроса is_public """
-
         if self.request.query_params.get('is_public'):
             return Habit.objects.filter(is_public=True)
         return super().get_queryset()
 
 
-class SendRemindersView(APIView):
-    """ Отправляет напоминания о выполнении привычек в Telegram бот """
-
-    def post(self, request):
-        reminders = Habit.objects.filter(frequency=1, is_public=True)
-
-        for reminder in reminders:
-            # Отправить напоминание в Telegram бот
-            # send_reminder.delay(reminder.id)
-
-            return Response()
-
-# TODO: создать тесты
+# class SendRemindersView(APIView):
+#     """ Отправляет напоминания о выполнении привычек в Telegram бот """
+#
+#     def post(self, request):
+#         reminders = Habit.objects.filter(frequency=1, is_public=True)
+#
+#         for reminder in reminders:
+#             # Отправить напоминание в Telegram бот
+#             # send_reminder.delay(reminder.id)
+#
+#             return Response()
